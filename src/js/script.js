8efca14fad129ee2bf6ba002114fcadbdcaf168f
fabioferreira11 <145472440+fabioferreira11@users.noreply.github.com>
2024-01-08 10:56:54 +0100

    commite branche color-texte

--- src/js/script.js
+++ src/js/script.js
@@ -27,10 +27,10 @@ $(document).ready(function() {
 
                 // Afficher toutes les informations dans le conteneur HTML
                 $('#contenu').html(
-                    '<h2 class="citation text-center text-l">' + randomObject.Citation + ' </h2>' +
+                    '<h1 class="citation text-center text-white">' + randomObject.Citation + ' </h1>' +
                     '<div class="flex flex-row w-full justify-center items-center"><img src="' + randomObject.URLImage + '" alt="Auteur" class="my-4 rounded-full max-w-14">'+
-                    '<h2 class="auteur text-center mx-3">' + randomObject.Auteur + '</h2>' +
-                    '<p class="description text-center ml-3">' + randomObject.Description + '</p> </div>'
+                    '<p class="auteur text-center mx-3 text-white">' + randomObject.Auteur + '</p>' +
+                    '<p class="description text-center ml-3 text-gray-600">' + randomObject.Description + '</p> </div>'
                    
                 );
             } else {
